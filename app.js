308944ba88eeb821fa10d4c4076f65df5b589bdd
Hamza <hamza.dahmoun7@gmail.com>
2019-11-09 11:57:15 +0100

    adding dragstart event to player square

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 //First of all lets make the plyer square element draggable
 document.getElementById("player").setAttribute("draggable", true);
 
+//Now lets specify what should happen when the player square is dragged by adding ondragstart event
+document.getElementById("player").addEventListener("dragstart", colorizeRed);
+
+
