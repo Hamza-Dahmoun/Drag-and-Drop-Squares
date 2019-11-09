98b130db7978385859fe458b2fc9e741e1f12ce0
Hamza <hamza.dahmoun7@gmail.com>
2019-11-09 12:42:25 +0100

    making all squares droppable elements

--- app.js
+++ app.js
@@ -15,3 +15,19 @@ function make_playerRed_andCircle(event) {
     //store its id as a text in its data
     event.dataTransfer.setData("text", event.target.id);
 }
+
+
+//Second of all, lets show to user that all orange squares accepts dropping by colorizing with light green
+var squaresDroppable = document.getElementsByClassName("square");
+
+for (let i = 0; i < squaresDroppable.length; i++) {
+    squaresDroppable[i].addEventListener("dragover", allowDrop);
+}
+function allowDrop(event) {    
+    //making the target square droppable
+    event.preventDefault();
+    //making the drag and drop event doing the action of moving an element not copying it
+    event.dataTransfer.dropEffect = "move";
+    //making the square green so that the user understands it is droppable zone
+    event.target.style.backgroundColor = "#8beea8";
+}
