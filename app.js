32732b01b495d030585aa992abd1fc4d2abb0fb7
Hamza <hamza.dahmoun7@gmail.com>
2019-11-09 12:41:40 +0100

    fix dragstart func

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
+//This variable will always hold the text that have been erased by the draggable element
+var oldText="";
+
 //First of all lets make the plyer square element draggable
 document.getElementById("player").setAttribute("draggable", true);
 
 //Now lets specify what should happen when the player square is dragged by adding ondragstart event
 document.getElementById("player").addEventListener("dragstart", make_playerRed_andCircle);
 
-function make_playerRed_andCircle(event){
+function make_playerRed_andCircle(event) {
+    //make it red
     event.target.style.backgroundColor = "red";
+    //make it circle
     event.target.style.borderRadius = "50%";
-}
-
-//Second of all, lets show to user that all orange squares accepts dropping by colorizing with light green
-var squaresDroppable = document.getElementsByClassName("square");
-for(let i = 0; i< squaresDroppable.length; i++){
-    squaresDroppable[i].addEventListener("dragover", makeSquareLightGreen);
-}
-
-function makeSquareLightGreen(event){
-    event.target.style.backgroundColor = "#A9F5BC";
+    //store its id as a text in its data
+    event.dataTransfer.setData("text", event.target.id);
 }
