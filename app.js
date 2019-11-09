07f9117de99934724270a10085914f301100e903
Hamza <hamza.dahmoun7@gmail.com>
2019-11-09 11:59:16 +0100

    write dragstart func make_playerRed_andCircle()

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 document.getElementById("player").setAttribute("draggable", true);
 
 //Now lets specify what should happen when the player square is dragged by adding ondragstart event
-document.getElementById("player").addEventListener("dragstart", colorizeRed);
+document.getElementById("player").addEventListener("dragstart", make_playerRed_andCircle);
 
-
+function make_playerRed_andCircle(event){
+    event.target.style.backgroundColor = "red";
+    event.target.style.borderRadius = "50%";
+}
